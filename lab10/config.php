f6abfb7744dfc0f583c4cbe930f7efe703d84ae8
Jio Kim <19302016002@fudan.edu.cn>
2020-06-07 23:26:59 +0800

    0607

---
+++ lab10/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', 'JIOKIM');
+define('DBPASS', 'sibadapttzw123');
+define('DBCONNSTRING','mysql:dbname=art;charset=utf8mb4;');
+?>
